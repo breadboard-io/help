b8ad7dad8f672abb775e12b858ab0502cfc44557
Ahmed Kamel <ak@ahmedkamel.net>
2018-07-21 01:05:23 -0700

    email example

--- examples/email.js
+++ examples/email.js
@@ -13,11 +13,11 @@ module.exports = function(ctx, res){
   let body = {
       to      : 'hello@breadboard.io'
     , from    : 'hello@breadboard.io'
-    , subject : 'subject: ' + req.body
-    , text    : req.body
+    , subject : `subject: ${ctx.body}`
+    , text    : ctx.body
   };
 
   return sdk
           .function('https://github.com/breadboard-io/functions/blob/master/lib/email.js')
-          .call({ ctx, body, forward_auth });
+          .call({ ctx, body, authorization : true });
 };
